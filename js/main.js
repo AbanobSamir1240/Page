e70f8c7b75e2ca9f78da772ad6fff42af8b5b7b7
AbanobSamir <abanob.samir1240@gmail.com>
2020-07-19 14:51:52 +0200

    PageUpdate

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@
 $("#carouselExampleControls6").carousel({
   interval: false,
 });
+$("#timeline").timelinev({
+  containerDivs: ".timelineCont",
+  dateDiv: ".date",
+  textDiv: ".text",
+  dateHtml: "h5",
+  textHtml: "p",
+  dateActiveClass: ".active",
+});
